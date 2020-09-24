d11f26547ed29bfa273deedb399d0e86ff48dbb1
CristianCaro915 <c.caro@uniandes.edu.co>
2020-09-23 22:34:57 -0500

    eer

--- App/view.py
+++ App/view.py
@@ -56,11 +56,11 @@ def PrintRQ1(compa):
     print('Productora es: ' + compa['producer'])
     print('Promedio es: ' + str(compa['average'][1]))
     print('Total de libros: ' + str(lt.size(compa['movies'])))
-    iterator = it.newIterator(compa)
+    iterator = it.newIterator(compa["movies"])
     i=0
     while it.hasNext(iterator) and i<6:
         peli = it.next(iterator)
-        print("Título #" + str(i) + str(peli["movies"]) )
+        print("Título #" + str(i) +" "+ str(peli) )
         i+=1
 
 def PrintRQ2(direc):
